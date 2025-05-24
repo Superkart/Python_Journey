b92b1c2139d8818207768adf510318fdccf62801
Superkart <karthik.ragi15@gmail.com>
2025-05-24 01:39:55 -0500

    Added a method to remove an element in a linked list

--- Python_Tutorials/EX2VD4/linkedList.py
+++ Python_Tutorials/EX2VD4/linkedList.py
@@ -26,17 +26,17 @@ class LinkedList:
         print(llstr)
     
     def insert_At_End(self, data):
-        if self.head is None:
+        if self.head is None: # CASE WHEN THE LINKED LIST IS EMPTY
             self.head = Node(data, None)
             return
-        itr = self.head
+        itr = self.head # GENERAL CASE
         while itr.next:
             itr = itr.next
         
         itr.next = Node(data, None)
 
     def insert_By_Values(self, data_list):
-        self.head = None
+        self.head = None 
         for data in data_list:
             self.insert_At_End(data)
             
@@ -47,11 +47,30 @@ class LinkedList:
             count = count + 1
             itr = itr.next
         print(count)
+        return count
 
-    
+    def remove_Node(self, index):
+        if(index < 0 or index >= self.get_Length()): # CASE WHEN INDEX IS NOT IN THE LINKEDLIST
+            raise Exception("Invalid index")
+        
+        if(index == 0): # CASE WHEN INDEX IS THE FIRST ELEMENT
+            self.head = self.head.next
+
+        count = 0 # GENERAL CASE 
+        itr = self.head
+        while itr:
+            if(count == index - 1):
+                itr.next = itr.next.next
+                break
+
+            itr = itr.next
+            count = count + 1
+             
+
 
 if __name__ == '__main__':
     ll = LinkedList()
     ll.insert_By_Values(["Ragi Janardhan", "Ragi Vinay Kumar", "Ragi Karthik"])
+    ll.remove_Node(0)
     ll.print_LinkedList()
     ll.get_Length()
