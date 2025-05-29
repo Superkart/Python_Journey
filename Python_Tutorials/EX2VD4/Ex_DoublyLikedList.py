ba831c59be3e44be1964a839ff5a39863a5a93c7
Superkart <karthik.ragi15@gmail.com>
2025-05-28 22:31:01 -0500

    Added new method to add element at end of Linked list and fixed insert at begining

--- Python_Tutorials/EX2VD4/Ex_DoublyLikedList.py
+++ Python_Tutorials/EX2VD4/Ex_DoublyLikedList.py
@@ -16,17 +16,18 @@ class DoublyLinkedList:
         if(not self.head): # CASE WHERE LINKED LIST IS EMPTY
             self.head = node
         else: # CASE WHERE LINKED LIST HAS ELEMENTS
+            self.head.prev = node
             node.next = self.head
             self.head = node
 
     def getLength(self):
         count = 0
         itr = self.head
-        while itr:
+        while itr.next:
             count = count + 1
             itr = itr.next
-        print(count)
-        return count
+        print(count + 1)
+        return count + 1
 
     def print_LinkedList_Forward(self):
         if(self.head == None):
@@ -45,16 +46,26 @@ class DoublyLinkedList:
         llstr = ""
         itr = self.head
         lastnode = None
-        while itr:
+        while itr.next:
             itr = itr.next
-        lastnode = itr
-        itr = lastnode
+
+        print(str(itr.data) + " This is the last node")
         while itr:
             llstr = llstr + str(itr.data) + "<---"
             itr = itr.prev
 
         print(llstr)        
 
+    def insert_At_End(self, data):
+        node = Node(data, None, None)
+        if(self.head == None):
+            self.head = node
+        else:
+            itr = self.head
+            while itr.next:
+                itr = itr.next
+            itr.next = node
+            node.prev = itr
 
 
 if __name__ == "__main__":
@@ -62,5 +73,7 @@ if __name__ == "__main__":
     Dll.insert_At_Begining("Ragi Janardhan")
     Dll.print_LinkedList_Forward()
     Dll.insert_At_Begining("Ragi Vinay Kumar")
-
+    Dll.insert_At_End("Karthik Ragi")
     Dll.print_LinkedList_Forward()
+    Dll.print_LinkedList_Backward()
+    Dll.getLength()
