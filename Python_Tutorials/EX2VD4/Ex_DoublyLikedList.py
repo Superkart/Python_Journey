586d755ffcdac08cc8b5667e0db9d11c6207a004
Superkart <karthik.ragi15@gmail.com>
2025-06-02 12:44:13 -0500

    Added a new method to insert an element after a value specified

--- Python_Tutorials/EX2VD4/Ex_DoublyLikedList.py
+++ Python_Tutorials/EX2VD4/Ex_DoublyLikedList.py
@@ -49,7 +49,7 @@ class DoublyLinkedList:
         while itr.next:
             itr = itr.next
 
-        print(str(itr.data) + " This is the last node")
+        #print(str(itr.data) + " This is the last node")
         while itr:
             llstr = llstr + str(itr.data) + "<---"
             itr = itr.prev
@@ -73,10 +73,29 @@ class DoublyLinkedList:
         for data in data_list:
             self.insert_At_End(data)
              
+    def insert_After_Value(self, after_value, data):
+        if(self.head == None):
+            raise Exception("Invalid request")
+        
+        itr = self.head
+
+        while itr:
+            if(itr.data == after_value):
+                node = Node(data, itr, itr.next)
+                if itr.next:
+                    itr.next.prev = node
+                itr.next = node
+                return
+            itr= itr.next
+        print("Value doesnt exist")
+
+
+        
 
 if __name__ == "__main__":
     Dll = DoublyLinkedList()
     Dll.insert_By_Values(["Karthik Ragi", "Ragi Vinay Kumar", "Ragi Janardhan"])
+    Dll.insert_After_Value("Karthik Ragi", "Divya Ragi")
     #Dll.insert_At_Begining("Ragi Janardhan")
     #Dll.insert_At_Begining("Ragi Vinay Kumar")
     #Dll.insert_At_End("Karthik Ragi")
